5647defc0066c5ebc03c08c4381f0e07e7f6763c
Willem-Baeyens <willem.baeyens@student.howest.be>
2025-06-09 01:54:09 +0200

    added events(observer pattern) hardcoded events to only work with void return type

---
+++ Minigin/Event.h
@@ -0,0 +1,44 @@
+#ifndef EVENT_H
+#define EVENT_H
+#include <vector>
+#include <algorithm>
+
+//TODO consider cleanup after bound object gets deleted
+template<class... ArgsTypes>
+class Event
+{
+public:
+	void BindFunction(const std::function<void(ArgsTypes...)>& function)
+	{
+		m_Function = function;
+	}
+
+	void Broadcast(ArgsTypes... args)
+	{
+		m_Function(args...);
+	}
+
+private:
+	std::function<void(ArgTypes...)> m_Function{};
+};
+
+
+template<class... ArgsTypes>
+class MulticastEvent
+{
+public:
+	void BindFunction(std::function<void(ArgsTypes...)> function)
+	{
+		m_Functions.push_back(function);
+	}
+
+	void Broadcast(ArgsTypes... args)
+	{
+		std::for_each(m_Functions.begin(), m_Functions.end(), [&args...](const std::function<void(ArgsTypes...)>& function) {function(args...); });
+	}
+private:
+	std::vector<std::function<void(ArgsTypes...)>> m_Functions{};
+};
+#endif // !EVENT_H
+
+
